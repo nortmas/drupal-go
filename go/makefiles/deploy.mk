d64452a30e63acb191003da24af5bcbe5b7a0a65
Dmitry Antonenko <dmitry@buzzwoo.de>
2019-02-14 12:43:55 +0700

    Apply new deployment flow.

--- go/makefiles/deploy.mk
+++ go/makefiles/deploy.mk
@@ -1,18 +1,18 @@
 ## Run commands needed for DEV deployment.
 go_dev_deploy:
-	$(call DRUPAL_PHP_COMPOSER, install)
+	$(call DRUPAL_PHP_COMPOSER, install --no-interaction)
 	$(call DRUPAL_PHP_ROBO, set_correct_permissions)
 	$(call DRUPAL_PHP_ROBO, rebuild)
 
 ## Run commands needed for STAGE deployment.
 go_stage_deploy:
-	$(call DRUPAL_PHP_COMPOSER, install)
+	$(call DRUPAL_PHP_COMPOSER, install --no-interaction)
 	$(call DRUPAL_PHP_ROBO, set_correct_permissions)
 	$(call DRUPAL_PHP_ROBO, rebuild)
 
 ## Run commands needed for PROD deployment.
 go_master_deploy:
-	$(call DRUPAL_PHP_COMPOSER, install)
+	$(call DRUPAL_PHP_COMPOSER, install --no-interaction)
 	$(call DRUPAL_PHP_ROBO, dbe)
 	$(call DRUPAL_PHP_ROBO, set_correct_permissions)
 	$(call DRUPAL_PHP_ROBO, rebuild)
