e17a2427132704bb83e66fb313fb41b39010a307
Dmitry Antonenko <raerayan@gmail.com>
2019-02-20 01:19:04 +0700

    .env fix

--- go/makefiles/go.mk
+++ go/makefiles/go.mk
@@ -1,3 +1,5 @@
+.SILENT:
+
 ## Include .env file if exists.
 ifneq ("$(wildcard .env)","")
     include .env
@@ -33,8 +35,8 @@ go_prepare_env:
 
 ## Check if the .env exists.
 go_check_env:
-	@[ -f acme.json ] && true || (touch acme.json && chmod 600 acme.json)
-	@[ -f .env ] && true || (echo "\033[31m File .env doesn't exist. Please run go_lin or go_mac commands. \033[0m" && exit 1)
+	[ -f acme.json ] && true || (touch acme.json && chmod 600 acme.json)
+	[ -f .env ] && true || (echo "\033[31m File .env doesn't exist. Please run go_lin or go_mac commands. \033[0m" && exit 1)
 
 ## Install Drupal.
 go_drupal_install:
@@ -54,16 +56,19 @@ go_run_in_php:
 go_mac:
 	make go_env
 	sed -i '' -e "2s/^//p; 2s/^.*/OS=macos-/" .env
-	sed -i '' -e "13s/^//p; 13s/^.*/PHP_XDEBUG_REMOTE_HOST=10.254.254.254/" .env
+	sed -i '' -e "12s/^//p; 12s/^.*/PHP_XDEBUG_REMOTE_HOST=host.docker.internal/" .env
+	if [ -f .env.extra ]; then cat .env.extra >> .env; fi
 
 ## Add specific settings for Linux to the .env file.
 go_lin:
 	make go_env
 	sed -i '2 i\OS=' .env
-	sed -i '13 a\PHP_XDEBUG_REMOTE_HOST=172.17.0.1' .env
+	sed -i '12 a\PHP_XDEBUG_REMOTE_HOST=172.17.0.1' .env
+	if [ -f .env.extra ]; then cat .env.extra >> .env; fi
 
 ## Create .env file using template.
 go_env:
+	if [ -f .env.extra ]; then rm -f .env; fi
 	cp -n go/templates/.env .env
 
 ## Kill php container.
